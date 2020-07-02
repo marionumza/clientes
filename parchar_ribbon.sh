c469b8719adb6c7f8b7303eaefba0101c9248fae
Mario Osvaldo Nuñez <marionumza@gmail.com>
2020-07-02 20:05:33 -0300

    1
    1

--- parchar_ribbon.sh
+++ parchar_ribbon.sh
@@ -1,2 +1,2 @@
-cd /home/odoo/odoo/11/clientes
-cp ribbon.css /home/odoo/odoo/11/sources/OCA-web/web_environment_ribbon/static/src/css/
+cd ~/server/11/sources/11/clientes
+cp ribbon.css ~/server/11/sources/11/sources/OCA-web/web_environment_ribbon/static/src/css/
